f73371897519a3dd046ac2475368215e9bc55018
renbing <970910609@qq.com>
2020-03-24 15:42:25 +0800

    优化总费用显示问题

--- app/src/main/java/com/example/myview/ChartView.kt
+++ app/src/main/java/com/example/myview/ChartView.kt
@@ -7,29 +7,31 @@ import android.util.AttributeSet
 import android.util.Log
 import android.view.View
 
-class MyView @JvmOverloads constructor(context: Context,attrs: AttributeSet? = null,defStyleAttr: Int = 0) : View(context,attrs,defStyleAttr) {
+class ChartView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : View(context,attrs,defStyleAttr) {
 
     //数据源
-    private var dataList: MutableList<Int> = mutableListOf()
+    private var dataList: MutableList<Double> = mutableListOf()
     private var hintList: MutableList<String> = mutableListOf()
     //颜色
     private var pureColors: MutableList<String> = mutableListOf()
 
     var heights: Int = 0
+    var widths: Int = 0
     //圆环在容器中到左边界的距离和到右边界的距离一样
     val ringOffset:Float = 90f
     //圆环半径
-    val ringRadius:Float = 160f
+    var ringRadius:Float = 160f
     //右边小圆圈半径
     val smallCircleRadius = 20f
     //大圆圆环宽度
-    val strokeWidth: Float =26f
+    val strokeWidth: Float =70f
     //大圆上下总间距
     val DISTANCE:Float =40f
     //用于计算半分比
-    var total: Int = 0
-    //大圆画笔
-    lateinit var bigCirclePaint: Paint
+    var total: Double = 0.0
+    //总费用金额文字大小
+    var totalTextSize = 40f
+
     lateinit var processCirclePaint:Paint
     lateinit var halfProcessCirclePaint:Paint
     lateinit var smallCirclePaint:Paint
@@ -45,16 +47,10 @@ class MyView @JvmOverloads constructor(context: Context,attrs: AttributeSet? = n
     }
 
     private fun initPaint() {
-        //底圆画笔
-        bigCirclePaint = Paint(Paint.ANTI_ALIAS_FLAG)
-        bigCirclePaint.style = Paint.Style.STROKE
-        bigCirclePaint.strokeWidth = strokeWidth
-        bigCirclePaint.color = Color.parseColor("#D81B60")
-        //        bigCirclePaint.setColor(Color.parseColor("#e9ecee"))
         //圆弧画笔
         processCirclePaint = Paint(Paint.ANTI_ALIAS_FLAG)
         processCirclePaint.style = Paint.Style.STROKE
-        processCirclePaint.strokeWidth = 70f
+        processCirclePaint.strokeWidth = strokeWidth
         processCirclePaint.color = Color.parseColor("#D81B60")
         //右边文字画笔
         txtPaint = TextPaint(Paint.ANTI_ALIAS_FLAG)
@@ -64,7 +60,7 @@ class MyView @JvmOverloads constructor(context: Context,attrs: AttributeSet? = n
         //中间大字画笔
         totalPaint = TextPaint(Paint.ANTI_ALIAS_FLAG)
         totalPaint.textAlign = Paint.Align.CENTER
-        totalPaint.textSize = 40f
+        totalPaint.textSize = totalTextSize
 
         halfProcessCirclePaint= Paint(Paint.ANTI_ALIAS_FLAG)
         smallCirclePaint= Paint(Paint.ANTI_ALIAS_FLAG)
@@ -77,10 +73,11 @@ class MyView @JvmOverloads constructor(context: Context,attrs: AttributeSet? = n
         pureColors.add("#3e34ff")
         pureColors.add("#fed74f")
         pureColors.add("#ff4091")
+        pureColors.add("#ff9081")
 
     }
 
-    fun setDataList(dataList:MutableList<Int>){
+    fun setDataList(dataList:MutableList<Double>){
         this.dataList = dataList
 
         /*  最开始求和这一步放在init里面的  init是在加载布局后就调用了  此时由于activity中数据还未加载完成 所以total会为0
@@ -116,10 +113,10 @@ class MyView @JvmOverloads constructor(context: Context,attrs: AttributeSet? = n
             var percent: Float
             if (index == dataList.size - 1) {
                 percent = 360 - recordAngle
-                if (total == 0)
+                if (total == 0.0)
                     percent = 0f
             } else {
-                percent = i * 1f / total * 360
+                percent = (i * 1f / total * 360).toFloat()
             }
             processCirclePaint.color = Color.parseColor(pureColors.get(index))
             canvas?.drawArc(rectF, recordAngle, percent, false, processCirclePaint)
@@ -129,9 +126,25 @@ class MyView @JvmOverloads constructor(context: Context,attrs: AttributeSet? = n
 
     private fun drawTotal(canvas: Canvas?) {
         totalPaint.typeface = Typeface.DEFAULT_BOLD
+        //超出时改变字体大小
+        changeTotalTxtSize()
         canvas?.drawText("￥" + total.toString(), ringOffset+ringRadius, heights/2*1f, totalPaint)
         totalPaint.typeface = Typeface.DEFAULT
         canvas?.drawText("总费用", ringOffset+ringRadius, heights/2*1f+dip2px(16f), totalPaint)
+    }
+
+    /**
+     * 总费用文字宽度超出圆环时 改变文字大小
+     */
+    private fun changeTotalTxtSize() {
+        val totalTxtWidth: Float = totalPaint.measureText("￥" + total.toString())
+        if (totalTxtWidth >= ringRadius * 2 - strokeWidth) {
+            totalPaint.textSize = totalPaint.textSize - 4
+            val totalTxtWidth1: Float = totalPaint.measureText("￥" + total.toString())
+            if (totalTxtWidth1 >= ringRadius * 2 - strokeWidth) {
+                totalPaint.textSize = totalPaint.textSize - 4
+            }
+        }
     }
 
     private fun drawRightInfo(canvas: Canvas?) {
@@ -146,7 +159,7 @@ class MyView @JvmOverloads constructor(context: Context,attrs: AttributeSet? = n
             val newY = startY + index * littleDis
             smallCirclePaint.color = Color.parseColor(pureColors[index])
             canvas?.drawCircle(cx, newY, smallCircleRadius, smallCirclePaint)
-            //画笔从左开始
+            //画笔从左开始  画金额
             txtPaint.textAlign = Paint.Align.LEFT
             canvas?.drawText(
                 "￥" + i,
@@ -154,11 +167,11 @@ class MyView @JvmOverloads constructor(context: Context,attrs: AttributeSet? = n
                 newY - ((txtPaint.descent() + txtPaint.ascent()) / 2f),
                 txtPaint
             )
-            //画笔从右开始
+            //画笔从右开始  画文字
             txtPaint.textAlign = Paint.Align.RIGHT
             canvas?.drawText(
                 hintList[index],
-                cx + dip2px(15f) + dip2px(130f),
+                cx + dip2px(155f),
                 newY - ((txtPaint.descent() + txtPaint.ascent()) / 2f),
                 txtPaint
             )
@@ -168,10 +181,10 @@ class MyView @JvmOverloads constructor(context: Context,attrs: AttributeSet? = n
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
 
-//        Log.e("TAG", h.toString())
         heights = h
-
-//        Log.e("TAG","高度："+dip2px(170f))
+        widths = w
+        //圆环半径设置为固定值  宽度的40%  超出时改字体大小
+        ringRadius = widths*0.4f/2 - dip2px(10f)
     }
 
     fun dip2px(dpValue : Float) : Int{
--- app/src/main/java/com/example/myview/MainActivity.kt
+++ app/src/main/java/com/example/myview/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.myview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -11,14 +10,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val myView = findViewById<MyView>(R.id.my_view)
+        val myView = findViewById<ChartView>(R.id.my_view)
         //金额数据源
-        val dataList:MutableList<Int> = mutableListOf()
-        dataList.add(1251600)
-        dataList.add(500000)
-        dataList.add(600000)
-        dataList.add(130200)
-        dataList.add(500000)
+        val dataList:MutableList<Double> = mutableListOf()
+        dataList.add(1251600.42)
+        dataList.add(500000.0)
+        dataList.add(600000.0)
+        dataList.add(130200.0)
+        dataList.add(500000.0)
+        dataList.add(400000.0)
         //文字数据源
         val hintList:MutableList<String> = mutableListOf()
         hintList.add("网店售价")
@@ -26,6 +26,7 @@ class MainActivity : AppCompatActivity() {
         hintList.add("技术年费")
         hintList.add("居间费")
         hintList.add("品牌授权押金")
+        hintList.add("品牌授权押金1")
         //绑定数据
         myView.setDataList(dataList)
         myView.setHintList(hintList)
